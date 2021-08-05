4908a11142e2ebddc90eb0a9646c816e6228c4bc
Vitali Krasnou <error404as@gmail.com>
2021-08-05 12:46:32 +0300

    html-pack: css, gulp update

--- html-gulp/gulpfile.js
+++ html-gulp/gulpfile.js
@@ -1,12 +1,13 @@
+// npm i -D gulp gulp-autoprefixer gulp-concat gulp-minify-css gulp-sass sass
+
 const { series, src, dest, watch } = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
-const minifyCss = require('gulp-minify-css');
 
 function css() {
     return src('src/css/base.scss')
-        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(sass({ outputStyle: 'compressed' }))
         .on('error', function (err) {
             console.log(err.toString());
             this.emit('end');
@@ -15,7 +16,6 @@ function css() {
             cascade: false
         }))
         .pipe(concat('styles.css'))
-        .pipe(minifyCss())
         .pipe(dest('public/css'));
 }
 
